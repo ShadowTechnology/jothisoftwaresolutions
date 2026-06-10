01a095ee01df526c0f8c1f586e4952b99545dfe2
Aravind S <aravindaravind400@gmail.com>
2026-06-10 07:15:29 +0530

    git ignore

--- next.config.mjs
+++ next.config.mjs
@@ -2,36 +2,6 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  // basePath: "/jothisoftwaresolutions",
-  // assetPrefix: "/jothisoftwaresolutions",
-
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.jothisoftwaresolutions.com",
-          },
-        ],
-        destination: "https://jothisoftwaresolutions.com/:path*",
-        permanent: true,
-      },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "header",
-            key: "x-forwarded-proto",
-            value: "http",
-          },
-        ],
-        destination: "https://jothisoftwaresolutions.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
 
   images: {
     unoptimized: true,
